b4e29f5b12c949ea7eda68eace4541be69c0fb4e
faranak6 <73706745+faranak6@users.noreply.github.com>
2023-07-03 15:16:52 +0330

    Add files via upload

--- uicolor/yui/yui.js
+++ uicolor/yui/yui.js
@@ -1,9 +1,4 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-
-/*jsl:ignoreall*/
+﻿/*jsl:ignoreall*/
 /*
 Copyright (c) 2009, Yahoo! Inc. All rights reserved.
 Code licensed under the BSD License:
